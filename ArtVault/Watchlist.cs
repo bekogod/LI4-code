93184cbecee31d235b6c38a24cfd40b68c97878d
myagi-ryota <a93285@alunos.uminho.pt>
2024-01-26 18:22:16 +0000

    Watchlist class

---
+++ ArtVault/Watchlist.cs
@@ -0,0 +1,18 @@
+﻿namespace ArtVault
+{
+    public class Watchlist
+    {
+        private HashSet<int> leiloes;
+
+        public Watchlist()
+        {
+            leiloes = [];
+        }
+        public HashSet<int> GetLeiloes() {  return leiloes; }
+
+        public bool AddLeilao(int id_leilao)
+        {
+            return leiloes.Add(id_leilao);
+        }
+    }
+}
