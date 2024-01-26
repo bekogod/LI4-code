7b7b22385660e13811ce28381c0726de610dc023
myagi-ryota <a93285@alunos.uminho.pt>
2024-01-26 17:09:49 +0000

    Abstract class Utilizador e class Padrao

---
+++ ArtVault/Utilizadores/Padrao.cs
@@ -0,0 +1,15 @@
+﻿namespace ArtVault.Utilizadores
+{
+    public class Padrao : Utilizador
+    {
+        public Padrao(int id, string email, string password, string username, string nome, string morada, string nif, string cc) 
+                   : base(id, email, password, username, nome, morada, nif, cc)
+        {
+        }
+
+        public bool verificarPassword(string password)
+        {
+            return this.password.Equals(password);
+        }
+    }
+}
---
+++ ArtVault/Utilizadores/Utilizador.cs
@@ -0,0 +1,36 @@
+﻿namespace ArtVault.Utilizadores
+{
+    public abstract class Utilizador
+    {   
+        protected int id;
+        protected string email;
+        protected string password;
+        protected string username;
+        protected string nome;
+        protected string morada;
+        protected string nif;
+        protected string cc;
+
+        protected Utilizador(int id, string email, string password, string username, string nome, string morada, string nif, string cc)
+        {
+            this.id = id;
+            this.email = email;
+            this.password = password;
+            this.username = username;
+            this.nome = nome;
+            this.morada = morada;
+            this.nif = nif;
+            this.cc = cc;
+        }
+
+        public int GetId() { return id; }
+        public string GetEmail() { return email; }
+        public string GetPassword() { return password; }
+        public string GetUsername() { return username; }
+        public string GetNome() { return nome; }
+        public string GetMorada() { return morada; }
+        public string GetNif() { return nif; }
+        public string GetCc() { return cc; }
+
+    }
+}
