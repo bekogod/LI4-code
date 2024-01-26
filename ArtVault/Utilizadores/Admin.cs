7fcc70ad0f08be8fb2af24a685e9bd0616dcee4d
myagi-ryota <a93285@alunos.uminho.pt>
2024-01-26 17:31:48 +0000

    class Admin

---
+++ ArtVault/Utilizadores/Admin.cs
@@ -0,0 +1,10 @@
+﻿namespace ArtVault.Utilizadores
+{
+    public class Admin : Utilizador
+    {
+        public Admin(int id, string email, string password, string username, string nome, string morada, string nif, string cc) 
+                : base(id, email, password, username, nome, morada, nif, cc)
+        {
+        }
+    }
+}
