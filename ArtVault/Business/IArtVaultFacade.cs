cad2ff8bb061310601640958425120b3aba62a9c
myagi-ryota <a93285@alunos.uminho.pt>
2024-01-30 23:47:13 +0000

    Contas de artista criadas inativas

--- ArtVault/Business/IArtVaultFacade.cs
+++ ArtVault/Business/IArtVaultFacade.cs
@@ -73,9 +73,11 @@ namespace ArtVault.Business
         public bool TryRegisto(string email, string password, string username, string nome, string morada, int nif, int cc, byte tipo)
         {
             int valid = IDBFacade.ExisteUtilizador(nif, cc, username, email);
+            bool active = true;
+            if (tipo == 2) { active = false; }
             if (valid == 0)
             {
-                IDBFacade.InsertUtilizador(username, password, email, nome, morada, nif, cc, tipo, true);
+                IDBFacade.InsertUtilizador(username, password, email, nome, morada, nif, cc, tipo, active);
                 return true;
             }
             return false;
